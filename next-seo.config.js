6403bd48355209d5e3d7d1d679cbd735bed79929
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-06-15 06:45:19 +0530

    upgraded seo and added fav icons

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,6 +11,35 @@ export default {
   },
   additionalLinkTags: [
     {
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon-32x32.png',
+      sizes: '32x32',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon-16x16.png',
+      sizes: '16x16',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#5bbad5',
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#da532c',
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff',
+    },
+    {
       rel: 'manifest',
       href: '/site.webmanifest',
     },
