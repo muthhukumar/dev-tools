cc2ff33f6aaa8cefa9104613f4cb9c6e829c68e9
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-06-15 08:05:25 +0530

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 
+import * as gtag from '../utils/gtags'
 import SEO from '../../next-seo.config'
-
 import theme from '../utils/theme'
 
+import type { AppProps } from 'next/app'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <DefaultSeo {...SEO} />
