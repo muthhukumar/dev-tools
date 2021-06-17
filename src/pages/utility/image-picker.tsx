054d7560d040ee12603bae0ae759687e2af0acbd
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-06-17 22:07:54 +0530

    updated the image pcker screen

--- src/pages/utility/image-picker.tsx
+++ src/pages/utility/image-picker.tsx
@@ -1,111 +1,128 @@
 import * as React from 'react'
-import { Box, Button, Container, Flex, Grid, Input } from '@chakra-ui/react'
+import { Text, Box, Button, Container, Flex, Grid, Input, Spinner } from '@chakra-ui/react'
 import { FiSearch } from 'react-icons/fi'
 import { NextSeo } from 'next-seo'
-import { useSWRInfinite } from 'swr'
 import _ from 'lodash'
 
 import { Page as PageFC } from '../../components/Page'
 import { Wrapper } from '../../components/Wrapper'
 import { GradientWrapper } from '../../components/GradientWrapper'
 import { Images } from '../../components/Images'
-// import { useDebounce } from '../../utils/hooks/useDebounce'
-
-const PAGE_SIZE = 20
-
-const fetcher = (url) => fetch(url).then((res) => res.json())
-
-const apiKey = process.env.NEXT_PUBLIC_PIXABAY
-
-type ImageProperties = {
-  id: number
-  largeImageURL: string
-  previewURL: string
-  imageHeight: number
-  imageWidth: number
-}
-
-type PixabayResponse = {
-  total: number
-  totalHits: number
-  hits: Array<ImageProperties>
-}
+import { useInfiniteImage, PAGE_SIZE } from '../../utils/hooks/useInfiniteImage'
 
 const RandomImageGenerator = () => {
   const [query, setQuery] = React.useState<string>('nature')
-  // const debounceQuery = useDebounce(query, 5000)
 
-  // console.log(debounceQuery)
+  const {
+    images,
+    debounceQuery,
+    isLoadingInitialData,
+    hasData,
+    isLoadingMore,
+    isReachingEnd,
+    setSize,
+  } = useInfiniteImage({ query })
 
-  // const { data, error, size, setSize, isValidating } = useSWRInfinite<PixabayResponse>(
-  const { data, error, size, setSize } = useSWRInfinite<PixabayResponse>(
-    // debounceQuery
-    (index) =>
-      `https://pixabay.com/api/?key=${apiKey}&orientation=horizontal&q=${query}&page=${
-        index + 1
-      }&per_page=${PAGE_SIZE}`,
-    // : null,
-    fetcher,
-  )
+  const handleDownloadImage = (imageFullUrl: string) => {
+    try {
+      const imageSplit = imageFullUrl.split('/') || []
 
-  const images = data ? [].concat(...data) : []
-  const isLoadingInitialData = !data && !error
-  const isLoadingMore =
-    isLoadingInitialData || (size > 0 && data && typeof data[size - 1] === 'undefined')
-  // const isEmpty = data?.[0]?.length === 0
-  // const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE)
-  // const isRefreshing = isValidating && data && data.length === size
+      const imageName = imageSplit[imageSplit.length - 1]
+
+      fetch(imageFullUrl, {
+        method: 'GET',
+      })
+        .then((response) => {
+          response.arrayBuffer().then(function (buffer) {
+            const url = window.URL.createObjectURL(new Blob([buffer]))
+            const link = document.createElement('a')
+            link.href = url
+            link.setAttribute('download', imageName) //or any other extension
+            document.body.appendChild(link)
+            link.click()
+          })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    } catch {}
+  }
 
   return (
     <PageFC>
       <NextSeo title="Image Selector" />
-      <Wrapper maxW="100%">
-        <Container maxW="container.lg">
-          <GradientWrapper height="80px" mt="16">
-            <Flex
-              borderColor="white.50"
-              borderWidth="1px"
-              w="100%"
-              h="100%"
-              px="4"
-              background={'black'}
-              rounded="xl"
-              alignItems="center"
-            >
-              <Input
-                variant="unstyled"
-                fontSize="2xl"
-                placeholder="Search..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-              />
-              <FiSearch size={30} />
-            </Flex>
-          </GradientWrapper>
+      <Wrapper maxW="100%" pt={0} h="100%">
+        <Container maxW="container.lg" h="100%">
+          <Box>
+            <GradientWrapper height={['60px', '65px', '75px', '80px']}>
+              <Flex
+                borderColor="white.50"
+                borderWidth="1px"
+                w="100%"
+                h="100%"
+                px="4"
+                background={'black'}
+                rounded="xl"
+                alignItems="center"
+              >
+                <Input
+                  variant="unstyled"
+                  fontSize="2xl"
+                  placeholder="Search..."
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+                <FiSearch size={30} />
+              </Flex>
+            </GradientWrapper>
+          </Box>
         </Container>
-        <Grid
-          templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']}
-          gap="2"
-          // w="100%"
-          mt="8"
-          p="10"
-        >
-          {images?.map((hits) => hits?.hits?.map((image) => <Images {...image} key={image.id} />))}
-        </Grid>
-        <Box w="100%" maxW="container.lg" mx="auto" my="12">
-          {/* {images?.hits?.length >= PAGE_SIZE && ( */}
-          <Button
-            w="100%"
-            variant="outline"
-            // disabled={isLoadingMore || isReachingEnd}
-            disabled={isLoadingMore}
-            onClick={() => setSize((oldSize) => oldSize + 1)}
-            isLoading={isLoadingMore}
+        {images.length === 0 && !query && (
+          <Text my="12" textAlign="center" fontSize="lg">
+            Start typing to search images.
+          </Text>
+        )}
+        {isLoadingInitialData ? (
+          <Flex w="100%" alignItems="center" justifyContent="center" my="12">
+            <Spinner />
+          </Flex>
+        ) : hasData ? (
+          <Grid
+            templateColumns={[
+              'repeat(1, 1fr)',
+              'repeat(2, 1fr)',
+              'repeat(3, 1fr)',
+              'repeat(4, 1fr)',
+            ]}
+            gap="2"
+            mt="8"
+            p={[2, 4, 6, 10]}
           >
-            {/* {isReachingEnd ? "You've reached the end" : 'Load more'} */}
-            Load more
-          </Button>
-          {/* )} */}
+            {images?.map((hits) =>
+              hits?.hits?.map((image) => (
+                <Images {...image} key={image.id} onDownload={handleDownloadImage} />
+              )),
+            )}
+          </Grid>
+        ) : (
+          debounceQuery && (
+            <Text my="12" textAlign="center" fontSize="lg">
+              No results found.
+            </Text>
+          )
+        )}
+        <Box w="100%" maxW="container.lg" mx="auto" my="12">
+          {images?.[0]?.hits?.length >= PAGE_SIZE && (
+            <Button
+              w="100%"
+              variant="outline"
+              disabled={isLoadingMore || isReachingEnd}
+              onClick={() => setSize((oldSize) => oldSize + 1)}
+              isLoading={isLoadingMore}
+            >
+              {isReachingEnd ? "You've reached the end" : 'Load more'}
+            </Button>
+          )}
         </Box>
       </Wrapper>
     </PageFC>
