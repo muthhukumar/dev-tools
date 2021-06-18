a0da00c5fd76f458bee36fd1ef6bf84c2e82d415
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-06-18 20:09:32 +0530

    updated the logic to calculate the array and then refactored the UI part

--- src/pages/utility/string-to-javascript-array.tsx
+++ src/pages/utility/string-to-javascript-array.tsx
@@ -4,58 +4,86 @@ import {
   Text,
   Button,
   Divider,
-  HStack,
   Input,
   InputGroup,
   InputLeftAddon,
   Textarea,
   useToast,
   VStack,
-  FormControl,
-  FormLabel,
-  Switch,
+  RadioGroup,
+  useRadioGroup,
+  Checkbox,
+  Heading,
+  Stack,
 } from '@chakra-ui/react'
+import _ from 'lodash'
 
 import { Page } from '../../components/Page'
 import { Wrapper } from '../../components/Wrapper'
 import { UtilityTitle } from '../../components/UtilityTitle'
 import { copyToClipboard } from '../../utils'
-import _ from 'lodash'
+import { SplitByRadioButton } from '../../components/RadioCard'
+
+const ONE = 'Single space'
+const TWO = 'New line'
+const THREE = 'Custom'
 
 const StringToJavascriptArray = () => {
   const textareaRef = React.useRef<HTMLTextAreaElement>(null)
   const outputTextareaRef = React.useRef<HTMLTextAreaElement>(null)
   const splitByInputRef = React.useRef<HTMLInputElement>(null)
-  const [wrapQuote, setWrapQuote] = React.useState<boolean>(false)
-  const [keepNumber, setKeepNumber] = React.useState(false)
-  const [removeEmptyCharacter, setRemoveEmptyCharacter] = React.useState(false)
+  const [wrapQuote, setWrapQuote] = React.useState<boolean>(true)
+  const [keepNumber, setKeepNumber] = React.useState<boolean>(false)
+  const [removeEmptyCharacter, setRemoveEmptyCharacter] = React.useState<boolean>(false)
+  const [splitByOption, setSplitByOption] = React.useState(ONE)
+
+  const options = [ONE, TWO, THREE]
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'Split by',
+    defaultValue: splitByOption,
+    onChange: setSplitByOption,
+  })
+
+  const group = getRootProps()
 
   const toast = useToast()
 
   const handleConvertToArray = () => {
     try {
-      const splitBy = splitByInputRef.current.value
+      const splitByOptions = {
+        [ONE]: ' ',
+        [TWO]: '\n',
+        [THREE]: splitByInputRef.current.value,
+      }
+
+      const splitBy = splitByOptions[splitByOption]
 
       const splitResult = textareaRef.current.value.split(splitBy)
 
-      const result = splitResult.map((entry) => {
+      const result = splitResult.map((text) => {
+        let formattedText = text
+
         if (wrapQuote) {
-          console.log(entry, !isNaN(+entry) && keepNumber)
-          if (entry !== '' && !isNaN(+entry) && keepNumber) {
-            return entry
-          }
-          return `'${entry}'`
+          formattedText = `'${formattedText}'`
         }
-        return entry
+
+        if (!isNaN(+text) && keepNumber) {
+          formattedText = text
+        }
+
+        if (text === '' && removeEmptyCharacter) {
+          return null
+        }
+
+        return formattedText
       })
 
-      const filteredResult = removeEmptyCharacter
-        ? result.filter((value) => {
-            return value !== "''"
-          })
-        : result
+      const filteredResult = result.filter((value) => value !== null)
 
       outputTextareaRef.current.textContent = `[${filteredResult}]`
+
+      outputTextareaRef.current.scrollIntoView()
     } catch {}
   }
   const copyOnFocus = () => {
@@ -77,51 +105,64 @@ const StringToJavascriptArray = () => {
       <Wrapper>
         <UtilityTitle mb="8">Convert string to javascript array</UtilityTitle>
         <Textarea as="textarea" rows={15} placeholder="Enter your text here" ref={textareaRef} />
-        <HStack alignItems="flex-end" mt="4" justifyContent="space-between">
-          <VStack>
+        <Stack
+          alignItems="flex-end"
+          mt="4"
+          justifyContent="space-between"
+          direction={['column', 'column', 'row', 'row']}
+        >
+          <VStack w="100%" alignItems="flex-start" mb={[12, 4, 0, 0]}>
+            <Heading mb="2" textAlign="left" w="100%">
+              Split by
+            </Heading>
+            <RadioGroup onChange={setSplitByOption} value={splitByOption}>
+              <SplitByRadioButton getRadioProps={getRadioProps} group={group} options={options} />
+            </RadioGroup>
             <InputGroup size="lg">
               <InputLeftAddon>Split By</InputLeftAddon>
-              <Input type="text" ref={splitByInputRef} />
+              <Input
+                type="text"
+                ref={splitByInputRef}
+                disabled={splitByOption !== THREE}
+                w={['100%', '100%', 'initial', 'initial']}
+              />
             </InputGroup>
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="wrap quote around text" mb="0">
-                Wrap quote around text
-              </FormLabel>
-              <Switch
-                id="wrap quote around text"
+            <VStack alignItems="flex-start" w="100%" pt="4">
+              <Heading mb="2">Options</Heading>
+              <Checkbox
                 size="lg"
+                colorScheme="gray"
                 isChecked={wrapQuote}
                 onChange={() => setWrapQuote((state) => !state)}
-              />
-            </FormControl>
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="keep number as number" mb="0">
-                Keep number as number
-              </FormLabel>
-              <Switch
-                id="keep number as number"
+              >
+                Wrap quote around text
+              </Checkbox>
+              <Checkbox
                 size="lg"
+                colorScheme="gray"
                 isChecked={keepNumber}
                 onChange={() => setKeepNumber((state) => !state)}
-              />
-            </FormControl>
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="remove empty character" mb="0">
-                Remove empty character
-              </FormLabel>
-              <Switch
-                id="remove empty character"
+              >
+                Keep number as number
+              </Checkbox>
+              <Checkbox
                 size="lg"
+                colorScheme="gray"
                 isChecked={removeEmptyCharacter}
                 onChange={() => setRemoveEmptyCharacter((state) => !state)}
-              />
-            </FormControl>
+              >
+                Remove empty character
+              </Checkbox>
+            </VStack>
           </VStack>
-
-          <Button size="lg" onClick={handleConvertToArray}>
+          <Button
+            size="lg"
+            onClick={handleConvertToArray}
+            w={['100%', '100%', 'initial', 'initial']}
+          >
             Convert
           </Button>
-        </HStack>
+        </Stack>
 
         <Divider mt="8" />
         <Text fontSize="4xl" textAlign="center" my="4">
