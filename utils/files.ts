005a1c74b28cdcd0404a200854665059e0a30fad
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-06-20 12:41:59 +0530

    initial setup

--- utils/files.ts
+++ utils/files.ts
@@ -1,10 +1,10 @@
 import fs from 'fs'
-import { join } from 'path'
+import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
 
 import type { Utility } from './types/utilitiy'
 
-const utilityDirectory = join(process.cwd(), '/pages/utility')
+const utilityDirectory = path.join(process.cwd(), '/pages/utility')
 
 export function getUtilitySlugs(): Array<Utility> {
   const files = fs.readdirSync(utilityDirectory)
@@ -17,3 +17,12 @@ export function getUtilitySlugs(): Array<Utility> {
     }
   })
 }
+
+// POSTS_PATH is useful when you want to get the path to a specific file
+export const SNIPPETS_PATH = path.join(process.cwd(), 'posts')
+
+// postFilePaths is the list of all mdx files inside the SNIPPETS_PATH directory
+export const snippetFilePaths = fs
+  .readdirSync(SNIPPETS_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path))
